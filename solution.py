1190ab2077b7f96f0b442e17c4c9425a0a782760
MaksunKar <130152425+MaksunKar@users.noreply.github.com>
2023-04-12 21:44:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-
 from scipy.stats import anderson_ksamp
 
 chat_id = 790154026  # Ваш chat ID, не меняйте название переменной
 
 
 def solution(x: np.array, y: np.array) -> bool:
-    alpha = 0.03
-    return anderson_ksamp([x, y]).pvalue < alpha
+    return anderson_ksamp([x, y]).pvalue <= 0.03  # Ваш ответ, True или False
+
