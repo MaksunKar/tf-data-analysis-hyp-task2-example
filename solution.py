bd27fcd31f011e5506e24e7307f3218cda02ea52
MaksunKar <130152425+MaksunKar@users.noreply.github.com>
2023-04-13 17:50:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,13 @@
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import ttest_ind
 
 chat_id = 790154026  # Ваш chat ID, не меняйте название переменной
 
 
 def solution(x: np.array, y: np.array) -> bool:
-    ks_stat, p_value = ks_2samp(x, y)
+    result = ttest_ind(x, y)
     alpha = 0.03
-    critical_value = np.sqrt(-0.5 * np.log(alpha / 2)) / np.sqrt(len(x) + len(y))
-
-    if ks_stat > critical_value:
+    if result[1] > alpha:
         return True
     else:
         return False
